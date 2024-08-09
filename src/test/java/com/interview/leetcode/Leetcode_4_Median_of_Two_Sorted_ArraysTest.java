0a4a9231241362787a1809b669168d05f379a6a3
CodeDaddy <giang.nq.it@gmail.com>
2024-08-09 19:12:39 -0400

    update leetcode4 solution1 code brute force merge and sort

--- src/test/java/com/interview/leetcode/Leetcode_4_Median_of_Two_Sorted_ArraysTest.java
+++ src/test/java/com/interview/leetcode/Leetcode_4_Median_of_Two_Sorted_ArraysTest.java
@@ -7,23 +7,44 @@ import static org.junit.jupiter.api.Assertions.*;
 class Leetcode_4_Median_of_Two_Sorted_ArraysTest {
 
     @Test
-    void findMedianSortedArrays_testcase1() {
+    void findMedianSortedArrays_Solution1_testcase1() {
         //given
         int[] nums1 = {1, 3};
         int[] nums2 = {2};
         //when
         Leetcode_4_Median_of_Two_Sorted_Arrays test = new Leetcode_4_Median_of_Two_Sorted_Arrays();
         //then
-        assertEquals(2.0, test.findMedianSortedArrays(nums1, nums2));
+        assertEquals(2.0, test.findMedianSortedArraysSolution1(nums1, nums2));
     }
     @Test
-    void findMedianSortedArrays_testcase2() {
+    void findMedianSortedArrays_Solution1_testcase2() {
         //given
         int[] nums1 = {1, 2};
         int[] nums2 = {3,4};
         //when
         Leetcode_4_Median_of_Two_Sorted_Arrays test = new Leetcode_4_Median_of_Two_Sorted_Arrays();
         //then
-        assertEquals(2.5, test.findMedianSortedArrays(nums1, nums2));
+        assertEquals(2.5, test.findMedianSortedArraysSolution1(nums1, nums2));
+    }
+
+    @Test
+    void findMedianSortedArrays_Solution2_testcase1() {
+        //given
+        int[] nums1 = {1, 3};
+        int[] nums2 = {2};
+        //when
+        Leetcode_4_Median_of_Two_Sorted_Arrays test = new Leetcode_4_Median_of_Two_Sorted_Arrays();
+        //then
+        assertEquals(2.0, test.findMedianSortedArraysSolution2(nums1, nums2));
+    }
+    @Test
+    void findMedianSortedArrays_Solution2_testcase2() {
+        //given
+        int[] nums1 = {1, 2};
+        int[] nums2 = {3,4};
+        //when
+        Leetcode_4_Median_of_Two_Sorted_Arrays test = new Leetcode_4_Median_of_Two_Sorted_Arrays();
+        //then
+        assertEquals(2.5, test.findMedianSortedArraysSolution2(nums1, nums2));
     }
 }
